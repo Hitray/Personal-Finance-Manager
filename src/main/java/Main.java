4e23a2f9aa4267052119c6864e2ff903d94cce21
Hitray <75577433+Hitray@users.noreply.github.com>
2022-10-06 07:45:29 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+import jsonData.JsonProductData;
 import product.ProductsTracker;
 
 import java.io.BufferedReader;
@@ -9,7 +11,7 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8989);) { 
+        try (ServerSocket serverSocket = new ServerSocket(8989);) 
             System.out.println("Server runs!");
 
             ProductsTracker productsTracker = new ProductsTracker();
@@ -17,23 +19,27 @@ public class Main {
                 try (
                         Socket socket = serverSocket.accept();
                         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 ) {
                     
                     String inString = in.readLine();
 
-                    
+                    // Формируем JSON из строки и добавляем данные в ProductTracer
+                    Gson gson = new Gson();
+                    JsonProductData jsonProductData = gson.fromJson(inString, JsonProductData.class);
+                    productsTracker.addNewProduct(jsonProductData);
 
-                    out.println("{" +
-                            "  \"maxCategory\": {" +
-                            "    \"category\": \"еда\"," +
-                            "    \"sum\": 350000" +
-                            "  }" +
-                            "}");
+                    
+                    String outJsonData = productsTracker.getJsonSumForCategoryByProductName(jsonProductData.title);
+
+                    System.out.println("Clietn json : ");
+                    System.out.println(outJsonData);
+
+                    out.println(outJsonData);
                 }
             }
         } catch (IOException e) {
-            System.out.println("Dont starts server");
+            System.out.println("Dont start server");
             e.printStackTrace();
         }
     }
